06915430a8ac8b8245585c28638cc1d76e427732
fattah amil <fattah.ptech2020@gmail.com>
2023-08-24 23:39:12 +0100

    restore workSpace

--- Front-end-Project/src/app/app.module.ts
+++ Front-end-Project/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './component/login-page/login-page.component';
+import { RegisterPageComponent } from './component/register-page/register-page.component';
+import { IndexPageComponent } from './component/index-page/index-page.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { CreatePostComponent } from './component/create-post/create-post.component';
+import { ShowPostComponent } from './component/show-post/show-post.component';
+import { ShowFollowersComponent } from './component/show-followers/show-followers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    IndexPageComponent,
+    NavBarComponent,
+    CreatePostComponent,
+    ShowPostComponent,
+    ShowFollowersComponent
   ],
   imports: [
     BrowserModule,
